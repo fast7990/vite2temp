a3bceabe84b05df8ae9f3f923b632b03e0500cf5
hsp_dev <fast7990@outlook.com>
2021-09-13 14:30:20 +0800

    i

--- src/view/login.vue
+++ src/view/login.vue
@@ -7,5 +7,19 @@
  * @LastEditTime: 2021-08-22 00:29:28
 -->
 <template>
-  <div>dddo</div>
+  <div>{{ app }}</div>
+  <div>{{ab}}</div>
+  <div>{{state.count}}</div>
 </template>
+<script setup>
+import { ref, reactive } from "vue";
+const state = reactive({ count: 0 });
+let app = ref(0);
+let ab=1
+const add = () => {
+  app.value++;
+  state.count++
+  ab++
+};
+add();
+</script>
